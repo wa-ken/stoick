38b0183639c3b561ddf412ab835b1bda6960eb65
zippo21 <leona.21ppo@gmail.com>
2015-04-30 22:01:55 +0900

    背景の色ランダム#2

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+	bgc = new Array();
+	bgc[0] = "#999999";
+	bgc[1] = "#ff9999";
+	bgc[2] = "#ff0000";
+	bgc[3] = "#99ff99";
+	bgc[4] = "#9999ff";
+	bgc[5] = "#0000ff";
+	n = Math.floor(Math.random()*bgc.length);
+	document.bgColor = bgc[n]
+});
