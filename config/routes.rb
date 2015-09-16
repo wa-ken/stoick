af6febbcd0d3c40b04dc64b25a267dc452397958
wa-ken <0905103t@gmail.com>
2015-09-16 11:10:29 +0000

    いったんuser関連全部削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   resources :questions
 
-  
-  devise_for :user, controllers: {
-    omniauth_callbacks: "users/omniauth_callbacks"
-  }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
